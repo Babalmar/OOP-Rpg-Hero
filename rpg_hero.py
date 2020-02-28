3f77533cc619d6d45cf7533a93df702cc689634c
Marcin Babalski <marcin.babalski@gmail.com>
2020-02-28 17:05:49 +0100

    adding Wizard / Rogue sub-classes

--- rpg_hero.py
+++ rpg_hero.py
@@ -9,21 +9,49 @@ class Hero:
 
 
 class Warrior(Hero):
-    def __init__(self, name, hp, attack, defence, experience, inventory):
+    def __init__(self, name, basic_class, hp, attack, defence, experience, inventory):
         super().__init__(name, hp, attack, defence, experience, inventory)
+        self.basic_class = basic_class
+
+    def __repr__(self):
+        return 'Name: {}\nClass: {}\nHit Points: {}\nAttack: {}\nDefence: {}\nExperience points: {}\nInventory: {}'\
+            .format(self.name, self.basic_class, self.hp, self.attack, self.defence, self.experience, self.inventory)
+
+    @staticmethod
+    def warcry():
+        print("Raaaa!")
 
 
-class Mage(Hero):
-    pass
+class Wizard(Hero):
+    def __init__(self, name, basic_class, hp, attack, defence, experience, inventory):
+        super().__init__(name, hp, attack, defence, experience, inventory)
+        self.basic_class = basic_class
+
+    def __repr__(self):
+        return 'Name: {}\nClass: {}\nHit Points: {}\nAttack: {}\nDefence: {}\nExperience points: {}\nInventory: {}' \
+            .format(self.name, self.basic_class, self.hp, self.attack, self.defence, self.experience, self.inventory)
+
+    @staticmethod
+    def chant():
+        print("Ka namaa fthan Cthulhu!")
 
 
-class Thief(Hero):
-    pass
+class Rogue(Hero):
+    def __init__(self, name, basic_class, hp, attack, defence, experience, inventory):
+        super().__init__(name, hp, attack, defence, experience, inventory)
+        self.basic_class = basic_class
+
+    def __repr__(self):
+        return 'Name: {}\nClass: {}\nHit Points: {}\nAttack: {}\nDefence: {}\nExperience points: {}\nInventory: {}' \
+            .format(self.name, self.basic_class, self.hp, self.attack, self.defence, self.experience, self.inventory)
 
 
-hero1 = Warrior('Conan', 30, 20, 15, 10000, {'sword', 'leather sandals'})
-print('Name: {}'.format(hero1.name))
-print('Attack: {}'.format(hero1.attack))
-print('Defence: {}'.format(hero1.defence))
-print('Inventory: {}'.format(hero1.inventory))
-print('Experience points: {}'.format(hero1.experience))
+hero1 = Warrior('Conan', 'Warrior', 30, 20, 15, 10000, {'sword', 'leather sandals'})
+hero2 = Wizard('Amarok', 'Wizard', 20, 14, 10, 11000, {'staff +1', 'robe'})
+
+hero1.warcry()
+hero2.chant()
+
+
+print(hero1)
+print(hero2)
