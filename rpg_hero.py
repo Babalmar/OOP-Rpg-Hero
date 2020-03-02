9be57bc207a32dd7e54f48b2122ef91a8e290b8a
Marcin Babalski <marcin.babalski@gmail.com>
2020-03-02 18:33:59 +0100

    Added 'Dice' class, name generator

--- rpg_hero.py
+++ rpg_hero.py
@@ -1,3 +1,34 @@
+import random
+
+
+class Dice:
+
+    @staticmethod
+    def k6():
+        k6 = random.randint(1, 6)
+        return k6
+
+    @staticmethod
+    def k8():
+        k8 = random.randint(1, 8)
+        return k8
+
+    @staticmethod
+    def k10():
+        k10 = random.randint(1, 10)
+        return k10
+
+    @staticmethod
+    def k12():
+        k12 = random.randint(1, 12)
+        return k12
+
+    @staticmethod
+    def k20():
+        k20 = random.randint(1, 20)
+        return k20
+
+
 class Hero:
     def __init__(self, name, hp, attack, defence, experience, inventory):
         self.name = name
@@ -7,29 +38,36 @@ class Hero:
         self.experience = experience
         self.inventory = inventory
 
+    def __repr__(self):
+        return 'Name: {}Hit Points: {}\nAttack: {}\nDefence: {}\nExperience points: {}\nInventory: {}'\
+            .format(self.name, self.hp, self.attack, self.defence, self.experience, self.inventory)
+
+    @staticmethod
+    def name():
+        names_list = []
+        with open('names.txt') as f:
+            for row in f:
+                names_list.append(row)
+        name = random.choice(names_list)
+        return name
+
 
 class Warrior(Hero):
-    def __init__(self, name, basic_class, hp, attack, defence, experience, inventory):
+    def __init__(self, name, hp, attack, defence, experience, inventory):
         super().__init__(name, hp, attack, defence, experience, inventory)
-        self.basic_class = basic_class
-
-    def __repr__(self):
-        return 'Name: {}\nClass: {}\nHit Points: {}\nAttack: {}\nDefence: {}\nExperience points: {}\nInventory: {}'\
-            .format(self.name, self.basic_class, self.hp, self.attack, self.defence, self.experience, self.inventory)
 
     @staticmethod
     def warcry():
         print("Raaaa!")
 
+    # @classmethod
+    # def recruit(cls):
+    #    return cls()
+
 
 class Wizard(Hero):
-    def __init__(self, name, basic_class, hp, attack, defence, experience, inventory):
+    def __init__(self, name, hp, attack, defence, experience, inventory):
         super().__init__(name, hp, attack, defence, experience, inventory)
-        self.basic_class = basic_class
-
-    def __repr__(self):
-        return 'Name: {}\nClass: {}\nHit Points: {}\nAttack: {}\nDefence: {}\nExperience points: {}\nInventory: {}' \
-            .format(self.name, self.basic_class, self.hp, self.attack, self.defence, self.experience, self.inventory)
 
     @staticmethod
     def chant():
@@ -37,22 +75,17 @@ class Wizard(Hero):
 
 
 class Rogue(Hero):
-    def __init__(self, name, basic_class, hp, attack, defence, experience, inventory):
+    def __init__(self, name, hp, attack, defence, experience, inventory):
         super().__init__(name, hp, attack, defence, experience, inventory)
-        self.basic_class = basic_class
-
-    def __repr__(self):
-        return 'Name: {}\nClass: {}\nHit Points: {}\nAttack: {}\nDefence: {}\nExperience points: {}\nInventory: {}' \
-            .format(self.name, self.basic_class, self.hp, self.attack, self.defence, self.experience, self.inventory)
 
     @staticmethod
     def sneak():
         print("...")
 
 
-hero1 = Warrior('Conan', 'Warrior', 30, 20, 15, 10000, {'sword', 'leather sandals'})
-hero2 = Wizard('Amarok', 'Wizard', 20, 14, 10, 11000, {'staff +1', 'robe'})
-hero3 = Rogue('Garrett', 'Thief', 22,17, 16, 12000, {'blackjack', 'bow'})
+hero1 = Warrior(Hero.name(), Dice.k20(), Dice.k12(), Dice.k10(), 10000, {'sword', 'leather sandals'})
+hero2 = Wizard(Hero.name(), Dice.k10(), Dice.k6(), Dice.k8(), 11000, {'staff +1', 'robe'})
+hero3 = Rogue(Hero.name(), Dice.k12(), Dice.k8(), Dice.k10(), 12000, {'blackjack', 'bow'})
 
 hero1.warcry()
 hero2.chant()
